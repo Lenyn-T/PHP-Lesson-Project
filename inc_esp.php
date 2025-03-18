c204ee83cdb1d7dd0b8f371df491eba6aaa6bb10
Lenyn-T <lenyn.taka@gmail.com>
2025-03-18 20:24:54 -0300

    last commit with inc_esp.php unsaved, now saved.

--- inc_esp.php
+++ inc_esp.php
@@ -1,21 +1,9 @@
 <?php
-    $servidor='localhost';
-    $usuario='root';
-    $senha='';
-    $db='clinica';
-    $con =mysqli_connect($servidor, $usuario, $senha, $db);
-    if (!$con) {
-        print("Erro na conexão com MySQL");
-        print("Erro: ".mysqli_connect_error());
-        exit;
-    }
-
     $nome = $_POST['nome'];
     $sigla = $_POST['sigla'];
     include ('conexao.php');
 
-    $query= "INSERT INTO especialidade (descricao, sigla) 
-    VALUES ('$nome', '$sigla')"; 
+    $query= "INSERT INTO especialidade (descricao, sigla) VALUES ('$nome', '$sigla')"; 
     $resu= mysqli_query($con, $query);
     If (mysqli_insert_id ($con)) {
         echo "<br><font color=blue> Inclusão realizada com sucesso !!</font>";    
